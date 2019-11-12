16c4a2a1e4a9c559a90bfc413b4b0c3cb2993730
antonredko <antonredko11116@gmail.com>
2019-11-12 17:58:16 +0200

    fast all

--- js/script.js
+++ js/script.js
@@ -1,26 +1,63 @@
 const diplomLink = document.getElementsByClassName('diplomView')[0].
-                            getElementsByTagName('a')[0];
+                            getElementsByTagName('a')[0],
+    additionLink = document.getElementsByClassName('diplomView')[0].
+                            getElementsByTagName('a')[2];
 
 diplomLink.addEventListener('click', showDiplom);
+additionLink.addEventListener('click', showAddition);
 
-function showDiplom(e) {
-    e.preventDefault();
+function hideChildren() {
     for(i = 0; i < document.body.children.length; i += 1) {
         document.body.children[i].style.display = 'none';
     }
+}
+
+function showDiplom(e) {
+    e.preventDefault();
+    hideChildren();
     let image = document.createElement('img');
         image.setAttribute('src', 'img/diplom.jpg');
-        image.style.margin = '0 auto';
-        image.style.width = '100%';
-        image.style.height = '100%';
+        image.classList.add('image-js');
     let close = document.createElement('a');
-        close.innerText = 'Закрити (X)';
+        close.innerText = 'ЗАЧИНИТИ';
         close.href = 'index.html';
-        close.style.textDecoration = 'none';
-        close.style.color = '#000000';
+        close.classList.add('close-js');
     document.body.style.textAlign = 'center';
     document.body.style.paddingTop = '20px';
     document.body.appendChild(close);
     document.body.appendChild(image);
-    
+}
+
+function showAddition(e) {
+    e.preventDefault();
+    hideChildren();
+    let image = document.createElement('img');
+        image.src = 'img/addition.png';
+        image.classList.add('image-js');
+    let close = document.createElement('a');
+        close.innerText = 'ЗАЧИНИТИ';
+        close.href = 'index.html';
+        close.classList.add('close-js');
+    let arrowLeft = document.createElement('span');
+        arrowLeft.innerHTML = "&lArr;";
+        arrowLeft.classList.add('arrow-js');
+        arrowLeft.addEventListener('click', function() {
+            if(image.src = 'img/addition2.jpg') image.src = 'img/addition.png';
+        });
+    let arrowRight = document.createElement('span');
+        arrowRight.innerHTML = "&rArr;"
+        arrowRight.classList.add('arrow-js');
+        arrowRight.addEventListener('click', function() {
+            if(image.src = 'img/addition.png') image.src = 'img/addition2.jpg';
+        });
+    let div = document.createElement('div');
+        div.classList.add('div-js');
+        div.appendChild(arrowLeft);
+        div.appendChild(image);
+        div.appendChild(arrowRight);
+    document.body.style.textAlign = 'center';
+    document.body.style.paddingTop = '20px';
+    document.body.appendChild(close);
+    document.body.appendChild(div);
+
 }
